e9b08fd6ca128295dddbb928d8c8d4374852979b
Jan Boddez <jan.boddez@gmail.com>
2019-12-25 16:15:46 +0100

    Add GitHub Updater URI

--- share-on-pixelfed.php
+++ share-on-pixelfed.php
@@ -1,13 +1,14 @@
 <?php
 /**
- * Plugin Name: Share on Pixelfed
- * Description: Easily share WordPress (image) posts on Pixelfed.
- * Author: Jan Boddez
- * Author URI: https://janboddez.tech/
- * License: GNU General Public License v3
- * License URI: http://www.gnu.org/licenses/gpl-3.0.html
- * Text Domain: share-on-pixelfed
- * Version: 0.1
+ * Plugin Name:       Share on Pixelfed
+ * Description:       Easily share WordPress (image) posts on Pixelfed.
+ * Author:            Jan Boddez
+ * Author URI:        https://janboddez.tech/
+ * GitHub Plugin URI: https://github.com/janboddez/share-on-pixelfed
+ * License:           GNU General Public License v3
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:       share-on-pixelfed
+ * Version:           0.1
  *
  * @author  Jan Boddez <jan@janboddez.be>
  * @license http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
